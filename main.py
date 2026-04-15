d78c76c1f95f1f7b5e98dea0dcc2191c1bb83ef7
TraSuaMamTom <hezi09x@gmail.com>
2026-04-15 08:23:18 +0700

    test push error

--- main.py
+++ main.py
@@ -6,3 +6,5 @@ if __name__ == "__main__":
     input_csv = "./data/raw/StudentPerformanceFactors.csv"
     output_csv = "./data/processed/StudentPerformance_cleaned.csv"
     clean_data.clean_dataset(input_csv, output_csv)
+
+#test push
