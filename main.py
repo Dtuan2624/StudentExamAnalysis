99b5f5fba58367d7d07dd9fa046c039cbe6cd937
qhuy2011 <qhuy20112004@gmail.com>
2026-04-15 08:30:50 +0700

    add visualize

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import src
+import pandas as pd
 from src import clean_data
+from src import visualize
+
 
 # Example usage
 if __name__ == "__main__":
     input_csv = "./data/raw/StudentPerformanceFactors.csv"
     output_csv = "./data/processed/StudentPerformance_cleaned.csv"
     clean_data.clean_dataset(input_csv, output_csv)
+
+df = pd.read_csv(output_csv)
+visualize.plot_score_distribution(df)
+visualize.plot_study_vs_score(df)
+visualize.plot_correlation_heatmap(df)
